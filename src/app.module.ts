422bc2ce8a4e501bc851ba0a925937b1492bb4dc
Tran Minh Thanh <trmthanh220895@gmail.com>
2024-11-12 00:27:06 +0800

    xxxxxxxxxxxxxxx

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 // import { ChatModule } from './chat/chat.module';
-import { ChatModule } from './chat/chat.module';
-import { RealTimeModule } from './real_time/real_time.module';
+
+// import { RealTimeModule } from './real_time/real_time.module';
 import { PrismaService } from './prisma.service';
 import { UserModule } from './user/user.module';
 
 
 
-
+// RealTimeModule,
 
 @Module({
-  imports: [ ChatModule, RealTimeModule, UserModule],
+  imports: [  UserModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
